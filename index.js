429bbf2fb82ee69cc9f4dbb882a4b12f6d0b3b15
Rimba Sudarmadi <113418908+rimbasudarmadi@users.noreply.github.com>
2024-05-17 14:23:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const authRoutes = require('./routes/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(session({
